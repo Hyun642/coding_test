3ec7cdbe8235ef7cd29ae0bee5ac3e06a33d130e
Hyun <jjjtoy@naver.com>
2025-10-13 12:52:21 +0900

    PY - 섬총사(Fix)

--- py/Lv. 1/삼총사.py
+++ py/Lv. 1/삼총사.py
@@ -1,10 +1,7 @@
 def solution(number):
     answer = 0
-    for i in range(len(number)):
-        if i>2:
-            break
-        for j in range(i+1,len(number)):
-            
+    for i in range(len(number)-2):
+        for j in range(i+1,len(number)-1):
             for k in range(j+1,len(number)):
                 if(number[i]+number[j]+number[k] == 0):
                     answer += 1
