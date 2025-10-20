fd7d81402a930a6656dd548f7c54f180b7d3eb40
Hyun <jjjtoy@naver.com>
2025-10-20 11:37:04 +0900

    PY - 행렬의 덧셈

--- py/Lv. 1/행렬의 덧셈.py
+++ py/Lv. 1/행렬의 덧셈.py
@@ -1,18 +1,6 @@
-# 크기가 작은 부분문자열
-# 문제 설명
-# 숫자로 이루어진 문자열 t와 p가 주어질 때, t에서 p와 길이가 같은 부분문자열 중에서, 
-# 이 부분문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 횟수를 return하는 함수 solution을 완성하세요.
+def solution(arr1, arr2	):
+    return [[c1+c2 for c1,c2 in zip(i,j)] for i,j in zip(arr1,arr2)]
 
-# 예를 들어, t="3141592"이고 p="271" 인 경우, 
-# t의 길이가 3인 부분 문자열은 314, 141, 415, 159, 592입니다. 
-# 이 문자열이 나타내는 수 중 271보다 작거나 같은 수는 141, 159 2개 입니다.
+print(solution([[1,2],[2,3]], [[3,4],[5,6]]))
+print(solution([[1],[2]],	[[3],[4]]))
 
-
-
-def solution(t, p):
-    le_t = len(t)
-    le_p = len(p)
-    return sum(1 for i in range(le_t-le_p+1) if int(t[i:i+le_p]) <= int(p))
-
-
-print(solution("3141592",	"271"			))
