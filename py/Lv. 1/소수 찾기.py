9d6a6b1bfcd40a9a282d5fdd9dd82fdc3b0a4a71
Hyun <105111234+Hyun642@users.noreply.github.com>
2026-01-02 00:49:56 +0900

    PY - 소수 찾기

---
+++ py/Lv. 1/소수 찾기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0
+    for i in range(2,n+1):
+        isPrime= True
+        for j in range(2,int(i**0.5)+1):
+            if i%j == 0:
+                isPrime = False
+                break
+        if isPrime:
+            answer +=1
+    return answer
+
+
+
+print("답: ",solution(10))
+print("답: ",solution(5))
