6dbb7e4ce988d64ed919116a9abefcd9f26b7132
Hyun <105111234+Hyun642@users.noreply.github.com>
2026-01-09 23:37:14 +0900

    PY - 유연근무제

---
+++ py/Lv. 1/유연근무제.py
@@ -0,0 +1,27 @@
+def solution(schedules, timelogs, startday):
+    answer = 0
+    for hopetime,realtimes in zip(schedules ,timelogs):
+        
+        hopetime+=10
+        if hopetime % 100 >= 60:
+            hopetime = hopetime + 40
+
+        redcard=False
+        
+        for i, realtime in enumerate(realtimes):
+            today = (startday + i - 1)%7 
+            if today in [5,6]:
+                continue
+            elif  realtime > hopetime:
+                redcard = True
+                break
+        if not redcard:
+             answer+=1
+        
+    return answer
+
+
+print("답: ",solution([700, 800, 1100],	[[710, 2359, 1050, 700, 650, 631, 659], [800, 801, 805, 800, 759, 810, 809], [1105, 1001, 1002, 600, 1059, 1001, 1100]],	5))
+#3
+# print("답: ",solution([730, 855, 700, 720],	[[710, 700, 650, 735, 700, 931, 912], [908, 901, 805, 815, 800, 831, 835], [705, 701, 702, 705, 710, 710, 711], [707, 731, 859, 913, 934, 931, 905]],	1))
+#2
